ae21dfd8ed85875e47d4e84038b2331b68bb85e6
RYLiang18 <richard.y.liang@gmail.com>
2020-03-29 14:52:53 -0700

    i wanna die

---
+++ gst_frontend/build/precache-manifest.6d219261377b6883696dda5dcbd2c28b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d5750096331097c8d2e6e985d6093dc4",
+    "url": "/the_gst/index.html"
+  },
+  {
+    "revision": "9ef316f44c9a14c3c552",
+    "url": "/the_gst/static/css/2.de424728.chunk.css"
+  },
+  {
+    "revision": "0c035e15bbb848250d73",
+    "url": "/the_gst/static/css/main.9551d90a.chunk.css"
+  },
+  {
+    "revision": "9ef316f44c9a14c3c552",
+    "url": "/the_gst/static/js/2.24aa870f.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/the_gst/static/js/2.24aa870f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0c035e15bbb848250d73",
+    "url": "/the_gst/static/js/main.cc404a24.chunk.js"
+  },
+  {
+    "revision": "335e7d60c6dafa74917e",
+    "url": "/the_gst/static/js/runtime-main.7a9b75f5.js"
+  }
+]);
