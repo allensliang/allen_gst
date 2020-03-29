17c9108bd7409055b92475205e227cb11324c5e9
allensliang <allen.s.liang@gmail.com>
2020-03-29 15:22:13 -0700

    kk

---
+++ gst_frontend/build/precache-manifest.6fd642ec95f8cddd43af7e476182aaed.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "589fbcf81647054e5432d1c12a7c1a15",
+    "url": "/the_gst/index.html"
+  },
+  {
+    "revision": "7389abfcee28c24af4ed",
+    "url": "/the_gst/static/css/2.28c4475d.chunk.css"
+  },
+  {
+    "revision": "013d6d347e0c83eb89a0",
+    "url": "/the_gst/static/css/main.d21552fd.chunk.css"
+  },
+  {
+    "revision": "7389abfcee28c24af4ed",
+    "url": "/the_gst/static/js/2.e253a973.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/the_gst/static/js/2.e253a973.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "013d6d347e0c83eb89a0",
+    "url": "/the_gst/static/js/main.24aee5d2.chunk.js"
+  },
+  {
+    "revision": "335e7d60c6dafa74917e",
+    "url": "/the_gst/static/js/runtime-main.7a9b75f5.js"
+  }
+]);
--- gst_frontend/build/service-worker.js
+++ gst_frontend/build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/the_gst/precache-manifest.6d219261377b6883696dda5dcbd2c28b.js"
+  "/the_gst/precache-manifest.6fd642ec95f8cddd43af7e476182aaed.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -35,5 +35,5 @@ workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
 workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/the_gst/index.html"), {
   
-  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
+  blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
