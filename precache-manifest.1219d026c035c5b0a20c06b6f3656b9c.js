67412c20dda1f0eec0735c8806f6172b2eda2f24
allensliang <allen.s.liang@gmail.com>
2020-03-29 18:51:09 -0700

    Updates

--- precache-manifest.1219d026c035c5b0a20c06b6f3656b9c.js
+++ precache-manifest.1219d026c035c5b0a20c06b6f3656b9c.js
@@ -1,6 +1,6 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "e4087cf3ac0f1712575f17c133aab050",
+    "revision": "22a455c4b9802fb8fe5e9d17a48ede7b",
     "url": "/allen_gst/index.html"
   },
   {
@@ -8,7 +8,7 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/allen_gst/static/css/2.28c4475d.chunk.css"
   },
   {
-    "revision": "98ccda4ab4f14fee34d4",
+    "revision": "e12e61fae13fe9680a5c",
     "url": "/allen_gst/static/css/main.d21552fd.chunk.css"
   },
   {
@@ -20,8 +20,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/allen_gst/static/js/2.f48ad2bb.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "98ccda4ab4f14fee34d4",
-    "url": "/allen_gst/static/js/main.92bb1af8.chunk.js"
+    "revision": "e12e61fae13fe9680a5c",
+    "url": "/allen_gst/static/js/main.d266f3b1.chunk.js"
   },
   {
     "revision": "d6f9c3f8c17df8afe2e7",
